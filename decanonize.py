f1192262a80a29e8b4d38aad807555c1a96fec11
Lev Maximov <lev.maximov@gmail.com>
2010-06-19 00:45:02 +0700

    enable gestures

--- decanonize.py
+++
@@ -1,8 +0,0 @@
-import re
-KEY = '____________________________'
-with open('res.ini') as fin:
-    with open('res-final.ini', 'wt') as fout:
-        fin.readline()
-        for line in fin:
-            fout.write(re.sub('="%s"$' % KEY, '', line))
-
